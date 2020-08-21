e942d520099fcd979c103b26d35a1ffbbc27fb59
Documenter.jl <documenter@juliadocs.github.io>
2020-08-21 02:26:04 +0000

    build based on c5cf385

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuExample","category":"page"},{"location":"#JuExample","page":"Home","title":"JuExample","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuExample]","category":"page"},{"location":"#JuExample.test_func-Tuple{Any,Any}","page":"Home","title":"JuExample.test_func","text":"test_func(x,y)\n\nThis is a test function documentation\n\n\n\n\n\n","category":"method"}]
+}
