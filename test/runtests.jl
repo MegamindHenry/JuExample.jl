055766df9081e37c97fc7c6b61d10783fc04cb4d
Xuefeng Luo <hakunamatatahenry@gmail.com>
2020-08-21 03:49:12 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,6 @@ using Test
 @testset "JuExample.jl" begin
   @test JuExample.test_func(0,1) == 1
   @test JuExample.test_func(1,1) == 2
+  @test JuExample.test_func(10,1) == 11
   @test JuExample.test_func(2,3) == 5
 end
